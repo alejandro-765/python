2e33a3fddb7438bca7c890f3635faef4d0393e5b
alejandro-765 <107668497+alejandro-765@users.noreply.github.com>
2022-07-07 22:47:04 -0700

    Return
    loop

--- recursive.py
+++ recursive.py
@@ -1,4 +1,12 @@
 ## defining power function
 def power(x,n):
     ## if n is 1 i.e same number power to 1
-kj
+elif(n!=1):
+        ## in return statment it is calling itself power() function
+        return(x*power(x,n-1))
+
+x=float(input("Enter base Number (x): "))
+while True:
+    ## loop runs until user enters positive number
+    n=float(input("Enter exponential value (n): "))
+    if n >= 1:
